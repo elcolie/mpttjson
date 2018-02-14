9fad9164a099b2e58f822834fbb97d83c07e03ca
Jean <email@example.com>
2018-02-14 17:27:10 +0100

    corrected urls

--- risk/api/views.py
+++ risk/api/views.py
@@ -3,7 +3,6 @@ from rest_framework.generics import (
 	RetrieveAPIView,
 	)
 
-
 from risk.models import Risk
 from .serializers import (
     RiskDetailSerializer,
@@ -12,29 +11,24 @@ from .serializers import (
 	)
 
 
-# CFE METHOD   --------------------------------------------------------------
-
-# List of all risks (does not list children of children)
-# https://www.youtube.com/watch?v=6cakSZCDmIc&list=PLEsfXFp6DpzTOcOVdZF-th7BS_GYGguAS&index=16
-
-
-# Risk list
-class RiskListAPIView(ListAPIView):
-    queryset = Risk.objects.all()
-    serializer_class = RiskSerializer   
-
-
-# Nested serializer which lists children of children (or descendants)
-# https://www.youtube.com/watch?v=1Ii5yZLS1Jc&list=PLEsfXFp6DpzTOcOVdZF-th7BS_GYGguAS&index=17
-
+# Risk detail
 
 class RiskDetailAPIView(RetrieveAPIView):
     queryset = Risk.objects.all()
     serializer_class = RiskDetailSerializer    
 
 
+# Risk list (using nested serializer)
+# Source: https://www.youtube.com/watch?v=1Ii5yZLS1Jc&list=PLEsfXFp6DpzTOcOVdZF-th7BS_GYGguAS&index=17
 
-# Adding recursive package
+class RiskListAPIView(ListAPIView):
+    queryset = Risk.objects.all()
+    serializer_class = RiskSerializer   
+
+
+# Risk list (using the recursive package)
+# Source ()
+
 class TreeListAPIView(ListAPIView):
     queryset = Risk.objects.all()
     serializer_class = TreeSerializer   
